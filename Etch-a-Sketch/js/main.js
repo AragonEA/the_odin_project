c5575d8b658de743d13c3c8bce48295f90629bd0
Aragon Emanuel <aragon3m4@gmail.com>
2022-07-13 16:23:33 -0300

    Add makeRows

--- Etch-a-Sketch/js/main.js
+++ Etch-a-Sketch/js/main.js
@@ -1,1 +1,12 @@
+const container = document.getElementById("container");
 
+function makeRows(rows, columns) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', columns);
+  for (i = 0; i < (rows * columns); i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
