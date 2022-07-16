f0c0ad3e36a4299a273692d7a03001248b6fa460
Aragon Emanuel <aragon3m4@gmail.com>
2022-07-16 17:26:55 -0300

    Add event listener to grid items

--- Etch-a-Sketch/js/main.js
+++ Etch-a-Sketch/js/main.js
@@ -1,9 +1,19 @@
 const container = document.getElementById("container");
 const colorPicker = document.querySelector("#colorpicker");
+let colorPicked = '#000000';
+
+makeRows(16, 16);
+
+const gridItems = document.querySelectorAll('#container > div');
 
 colorPicker.addEventListener('change', (event) => {
   colorPicked = event.target.value;
 });
+
+gridItems.forEach((item) => {
+  item.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = colorPicked;
+  })
 });
 
 function makeRows(rows, columns) {
@@ -14,5 +24,3 @@ function makeRows(rows, columns) {
     container.appendChild(cell).className = "grid-item";
   };
 };
-
-makeRows(16, 16);
